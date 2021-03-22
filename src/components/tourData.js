3a7b77817d98eab7f68d24fe9d74e4c6b116bd65
CarlosPinta <cxpinta@espe.edu.ec>
2021-03-22 13:18:24 -0500

    Cambios de Infor

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -3,7 +3,7 @@ export const tourData = [
     id: 1,
     city: "nueva york",
     img: "./img/newyork.jpeg",
-    name: "new york bridge tour",
+    name: "Tour puente de Nueva York",
     info:
       "La ciudad tiene muchos lugares y edificios reconocidos por todo el mundo. Por ejemplo, la estatua de la Libertad, ubicada en la isla homónima, y la isla de Ellis, que recibió a millones de inmigrantes que llegaban a Estados Unidos a finales del siglo XIX y comienzos del XX. Wall Street ha sido uno de los principales centros mundiales de finanzas desde la Segunda Guerra Mundial y es la sede de la Bolsa de Nueva York. La ciudad también ha concentrado a muchos de los edificios más altos del mundo, entre los que se encuentran el edificio Empire State, el One World Trade Center, el edificio Chrysler, el 432 Park Avenue, la Trump Tower, el edificio Seagram y anteriormente las torres gemelas del World Trade Center, que fueron derribadas en los atentados del 11 de septiembre de 2001."
   },
@@ -11,7 +11,7 @@ export const tourData = [
     id: 2,
     city: "paris",
     img: "./img/paris.jpeg",
-    name: "paris lights tour",
+    name: "Tour luces de Paris",
     info:
       "Conocida también como la «Ciudad de la Luz» (la Ville lumière), es el destino turístico más popular del mundo, con más de 42 millones de visitantes extranjeros por año.9​ Entre los monumentos de la ciudad figuran la Torre Eiffel, la catedral de Notre Dame, la avenida de los Campos Elíseos, el Arco de Triunfo, la basílica del Sacré Cœur, el Palacio de Los Inválidos, el Panteón, el arco de la Defensa, la ópera Garnier y el barrio de Montmartre, entre otros. También museos como el Louvre, el Museo de Orsay y el Museo Nacional de Historia Natural de Francia, así como un extenso sistema de educación superior. París ocupa un lugar importante en el ámbito de la cultura, la gastronomía, la moda y el lujo."
   },
@@ -19,7 +19,7 @@ export const tourData = [
     id: 3,
     city: "Londres",
     img: "./img/london.jpeg",
-    name: "london royal palace tour",
+    name: "Tour Palacio Real",
     info:
       "Londres es una ciudad global, uno de los centros neurálgicos en el ámbito de las artes, el comercio, la educación, el entretenimiento, la moda, las finanzas, los medios de comunicación, la investigación, el turismo o el transporte.8​ Es el principal centro financiero del mundo9​10​11​ y una de las áreas metropolitanas con mayor PIB.12​13​ Londres es también una capital cultural mundial,14​15​16​17​ la ciudad más visitada considerando el número de visitas internacionales18​ y tiene el mayor sistema aeroportuario del mundo según el tráfico de pasajeros.19​ Asimismo, las 43 universidades de la ciudad conforman la mayor concentración de centros de estudios superiores de toda Europa.20​ En el año 2012 Londres se convirtió en la única ciudad en albergar la celebración de tres Juegos Olímpicos de Verano.21​"
   },
@@ -27,7 +27,7 @@ export const tourData = [
     id: 4,
     city: "tokyo",
     img: "./img/tokyo.jpeg",
-    name: "tokyo sushi tour",
+    name: "Tour sushi de Tokyo",
     info:
       "Con una población que supera los 40 millones de habitantes, se subdivide en 23 barrios (区 -ku); 26 ciudades (市 -shi); un distrito (郡 -gun) subdividido en tres pueblos (町 -chō o -machi) y una villa (村 -son o -mura); y cuatro subprefecturas (支庁 -shichō) subdivididas en dos pueblos y siete villas, que representan a varias pequeñas islas al sur de Honshu que se extienden más allá de 1800 km de Shinjuku, capital de la metrópoli y sede de la gobernación. El centro de Tokio, con sus 23 barrios, ocupa un tercio de la metrópoli, con una población cercana a los 13,23 millones de habitantes; esta área es lo que se conoce internacionalmente como la ciudad de Tokio. En su área metropolitana viven más de 40 millones de habitantes, lo que la convierte en la segunda mayor aglomeración urbana del mundo detrás de la de Guangzhou, en China. En 2015 fue elegida como la ciudad más segura del mundo por el periódico The Economist.!"
   }
