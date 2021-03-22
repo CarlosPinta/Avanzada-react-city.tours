14f241aac498a7832a8b23a6608b1f2e4bcf0a8f
CarlosPinta <cxpinta@espe.edu.ec>
2021-03-22 12:46:20 -0500

    primero

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import Navbar from '../src/components/Navbar/Navbar';
+import TourList from './components/TourList';
+function App() {
+
+  return (
+    <main>
+      <Navbar />
+      <TourList />
+    </main>
+    
+  );
+}
+
+export default App;
